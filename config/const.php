6c1b8c5d1bcd88cee65b6f924a42b24321146215
EC2 Default User <ec2-user@ip-172-31-17-222.us-east-2.compute.internal>
2020-01-08 09:40:41 +0000

    デプロイ

--- config/const.php
+++ config/const.php
@@ -2,6 +2,6 @@
 
 return[
     // 管理画面用のクッキー名称、セッションテーブル名
-    'session_cookie_admin' => env('SESSION_COOKIE_ADMIN', Str::slug(env('APP_NAME', 'laravel'), '_').'_session'),
+    'session_cookie_admin' => env('SESSION_COOKIE_ADMIN', 'portfolio_session'),
     'ssession_table_admin' => env('SESSION_TABLE_ADMIN'),
 ];
